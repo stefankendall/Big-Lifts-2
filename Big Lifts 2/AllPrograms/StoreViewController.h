63b569eb9be3f6e2d36fa9ac793ee1d2f59ac75a
Stefan Kendall <stefankendall@gmail.com>
2013-06-15 13:24:01 -0400

    Added buy wiring for store view controller

--- Big Lifts 2/AllPrograms/StoreViewController.h
+++ Big Lifts 2/AllPrograms/StoreViewController.h
@@ -4,5 +4,6 @@
 {}
 @property (weak, nonatomic) IBOutlet UIButton *barLoadingBuyButton;
 @property (weak, nonatomic) IBOutlet UIButton *barLoadingPurchasedButton;
+- (IBAction)buyBarLoadingButtonTapped:(id)sender;
 
 @end
--- Big Lifts 2/AllPrograms/StoreViewController.m
+++ Big Lifts 2/AllPrograms/StoreViewController.m
@@ -5,9 +5,27 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    [self hideShowBarLoadingBuyButton];
+}
+
+- (void)hideShowBarLoadingBuyButton {
     BOOL barLoadingPurchased = [[IAPAdapter instance] hasPurchased:@"barLoading"];
     [self.barLoadingBuyButton setHidden:barLoadingPurchased];
     [self.barLoadingPurchasedButton setHidden:!barLoadingPurchased];
 }
 
+- (IBAction)buyBarLoadingButtonTapped:(id)sender {
+    [[IAPAdapter instance] purchaseProductForId:@"barLoading" completion:^(NSObject *_){
+        UIAlertView *thanks = [[UIAlertView alloc] initWithTitle:@"Purchased!"
+                                                         message:@"Bar loading is now available throughout the app."
+                                                        delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [thanks performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
+        [self hideShowBarLoadingBuyButton];
+    }                                     error:^(NSError *err) {
+        UIAlertView *error = [[UIAlertView alloc] initWithTitle:@"Could not purchase..."
+                                                        message:@"Something went wrong trying to connect to the store. Please try again later."
+                                                       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [error performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
+    }];
+}
 @end
