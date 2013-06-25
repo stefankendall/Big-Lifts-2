2002bb42cf538bd9a855684ca720cd1d914a561d
Stefan Kendall <stefankendall@gmail.com>
2013-06-25 17:35:59 -0400

    Wired edit navigation on fto nav.

--- Big Lifts 2/531/FTONavController.m
+++ Big Lifts 2/531/FTONavController.m
@@ -1,5 +1,23 @@
+#import <ViewDeck/IIViewDeckController.h>
 #import "FTONavController.h"
 
 @implementation FTONavController
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Storyboard" bundle:nil];
+    [self.viewDeckController closeLeftViewAnimated:YES];
+
+    NSDictionary *tagViewMapping = @{
+            @1 : @"ftoEditNavController"
+    };
+
+    if ([cell tag] == 6) {
+        [[self.viewDeckController navigationController] popViewControllerAnimated:YES];
+    } else {
+        NSString *storyBoardId = [tagViewMapping objectForKey:[NSNumber numberWithInteger:[cell tag]]];
+        [self.viewDeckController setCenterController:[storyboard instantiateViewControllerWithIdentifier:storyBoardId]];
+    }
+}
 
 @end
