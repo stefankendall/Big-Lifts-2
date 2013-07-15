6905d009d9a0c1d1eef5645a265c330c14602dcd
Stefan Kendall <stefankendall@gmail.com>
2013-07-15 18:48:38 -0400

    Variant name can now be changed. Need to setup workout store data and tests.

--- Big Lifts 2/531/Plan/FTOPlanViewController.m
+++ Big Lifts 2/531/Plan/FTOPlanViewController.m
@@ -1,4 +1,5 @@
 #import <MRCEnumerable/NSArray+Enumerable.h>
+#import <MRCEnumerable/NSDictionary+Enumerable.h>
 #import "FTOPlanViewController.h"
 #import "FTOSettingsStore.h"
 #import "FTOSettings.h"
@@ -44,6 +45,20 @@
         settings.trainingMax = [NSDecimalNumber decimalNumberWithString:[textField text]];
     }
 }
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([indexPath section] == 1) {
+        UITableViewCell *selectedCell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
+        NSString *newVariantName = [self.variantCells detect:^BOOL(id key, UITableViewCell *cell) {
+            return cell == selectedCell;
+        }];
+        FTOVariant *variant = [[FTOVariantStore instance] first];
+        variant.name = newVariantName;
+
+        [self checkCurrentVariant];
+    }
+}
+
 
 - (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
     return [self emptyView];
--- Big Lifts 2Tests/531/Plan/FTOPlanViewControllerTests.m
+++ Big Lifts 2Tests/531/Plan/FTOPlanViewControllerTests.m
@@ -1,6 +1,8 @@
 #import "FTOPlanViewControllerTests.h"
 #import "FTOPlanViewController.h"
 #import "SenTestCase+ControllerTestAdditions.h"
+#import "FTOVariant.h"
+#import "FTOVariantStore.h"
 
 @implementation FTOPlanViewControllerTests
 
@@ -9,9 +11,16 @@
     STAssertFalse([[controller.trainingMaxField text] isEqualToString:@""], @"");
 }
 
-- (void) testChecksCurrentFtoVariant {
+- (void)testChecksCurrentFtoVariant {
     FTOPlanViewController *controller = [self getControllerByStoryboardIdentifier:@"ftoPlan"];
     STAssertEquals([controller.standardVariant accessoryType], UITableViewCellAccessoryCheckmark, @"");
+}
+
+- (void)testSelectVariant {
+    FTOPlanViewController *controller = [self getControllerByStoryboardIdentifier:@"ftoPlan"];
+    [controller tableView:controller.tableView didSelectRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:1]];
+    FTOVariant *variant = [[FTOVariantStore instance] first];
+    STAssertEqualObjects(variant.name, @"Pyramid", @"");
 }
 
 @end
