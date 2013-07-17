b0c62d9cd2f72955a5916eca1f42b6be28b030b8
Stefan Kendall <stefankendall@gmail.com>
2013-07-16 23:15:26 -0400

    Setup amrap set view. Need to create delegate for sneding reps data back to controller, wire push segue, and setup initial data.

---
+++ Big Lifts 2/531/Lift/Subviews/FTOAmrapForm.h
@@ -0,0 +1,6 @@
+@interface FTOAmrapForm : UITableViewController
+{}
+@property (weak, nonatomic) IBOutlet UITextField *repsField;
+@property (weak, nonatomic) IBOutlet UILabel *weightField;
+
+@end
---
+++ Big Lifts 2/531/Lift/Subviews/FTOAmrapForm.m
@@ -0,0 +1,5 @@
+#import "FTOAmrapForm.h"
+
+@implementation FTOAmrapForm
+
+@end
