7c60ce2d2733be5035c07e69f504b47bc7e4c445
Stefan Kendall <stefankendall@gmail.com>
2013-07-21 15:02:25 -0400

    Fixing amrap indicators

--- Big Lifts 2/StartingStrength/Lift/Individual/SetCell.h
+++ Big Lifts 2/StartingStrength/Lift/Individual/SetCell.h
@@ -8,6 +8,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *weightLabel;
 @property (weak, nonatomic) IBOutlet UILabel *repsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *liftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *optionalLabel;
 
 - (void)setSet:(Set *) set;
 @end
--- Big Lifts 2/StartingStrength/Lift/Individual/SetCell.m
+++ Big Lifts 2/StartingStrength/Lift/Individual/SetCell.m
@@ -7,16 +7,25 @@
 @implementation SetCell
 
 - (void)setSet:(Set *)set {
-    if ([[set reps] intValue] <= 0 && [set amrap]) {
+    int reps = [[set reps] intValue];
+    if (reps <= 0 && [set amrap]) {
         [self.repsLabel setText:@"AMRAP"];
     }
     else {
-        [self.repsLabel setText:[NSString stringWithFormat:@"%dx", [[set reps] intValue]]];
+        if ([set amrap]) {
+            [self.repsLabel setText:[NSString stringWithFormat:@"%d+", reps]];
+            [self.repsLabel setTextColor:[UIColor greenColor]];
+            [self setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+        }
+        else {
+            [self.repsLabel setText:[NSString stringWithFormat:@"%dx", reps]];
+        }
     }
 
     Settings *settings = [[SettingsStore instance] first];
     [self.weightLabel setText:[NSString stringWithFormat:@"%0.1f %@",
                                                          [[set roundedEffectiveWeight] doubleValue], settings.units]];
     [self.liftLabel setText:[set lift].name];
+    [self.optionalLabel setHidden:!set.optional];
 }
 @end
