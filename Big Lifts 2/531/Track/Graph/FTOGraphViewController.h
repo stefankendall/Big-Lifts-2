1c55213a3854d0f35bb2fc1a1cb3eaeeb70f7755
Stefan Kendall <stefankendall@gmail.com>
2013-07-27 13:23:54 -0400

    Added highcharts.

---
+++ Big Lifts 2/531/Track/Graph/FTOGraphViewController.h
@@ -0,0 +1,4 @@
+@interface FTOGraphViewController : UIViewController {}
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ Big Lifts 2/531/Track/Graph/FTOGraphViewController.m
@@ -0,0 +1,11 @@
+#import "FTOGraphViewController.h"
+
+@implementation FTOGraphViewController
+
+- (void)viewDidLoad {
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle]
+            pathForResource:@"graph" ofType:@"html"]                  isDirectory:NO]]];
+}
+
+
+@end
