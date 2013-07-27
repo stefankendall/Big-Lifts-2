19238f8307182b61f908c3abb6f7e74e527a5a76
Stefan Kendall <stefankendall@gmail.com>
2013-07-27 19:47:54 -0400

    Add transformer to turn objective c data into highcharts required format.

--- Big Lifts 2/531/Track/Graph/html/highcharts_transformer.js
+++ Big Lifts 2/531/Track/Graph/html/highcharts_transformer.js
@@ -1,3 +1,10 @@
-window.transformToHighChartsSeries = function(data){
-    return data;
+window.transformToHighChartsSeries = function (data) {
+    return _.map(data, function (liftObject) {
+        var newObject = {};
+        newObject.name = liftObject.name;
+        newObject.data = _.map(liftObject.data, function (record) {
+            return [Date.UTC(record.date.year, record.date.month - 1, record.date.day), record.weight];
+        });
+        return newObject;
+    });
 };
