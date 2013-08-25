aff33d48fa520f09de1e43f1a05ab3df1ee55d7d
Stefan Kendall <stefankendall@gmail.com>
2013-08-25 13:57:48 -0500

    Added facebook sharing wiring.

--- Big Lifts 2/531/LikeViewController.m
+++ Big Lifts 2/531/LikeViewController.m
@@ -1,16 +1,26 @@
 #import "LikeViewController.h"
+#import "FBAppCall.h"
+#import "FBDialogs.h"
 
 @implementation LikeViewController
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
-    if([cell viewWithTag:1]){
+    if ([cell viewWithTag:1]) {
         [self shareOnFacebook];
     }
 }
 
 - (void)shareOnFacebook {
-
+    NSURL *url = [NSURL URLWithString:@"https://itunes.apple.com/us/app/big-lifts-pro/id534996988?mt=8"];
+    [FBDialogs presentShareDialogWithLink:url
+                                  handler:^(FBAppCall *call, NSDictionary *results, NSError *error) {
+                                      if (error) {
+                                          NSLog(@"Error: %@", error.description);
+                                      } else {
+                                         NSLog(@"Neither of these get hit. Figure this out later.");
+                                      }
+                                  }];
 }
 
 
