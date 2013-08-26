5e3a5a6bf0622c0dc23bd8c09d97ca9f941ac475
Stefan Kendall <stefankendall@gmail.com>
2013-08-26 11:26:54 -0500

    BBB percentage can now be changed.

--- Big Lifts 2/531/Plan/Assistance/BBB/FTOBoringButBigViewController.m
+++ Big Lifts 2/531/Plan/Assistance/BBB/FTOBoringButBigViewController.m
@@ -1,10 +1,15 @@
 #import "FTOBoringButBigViewController.h"
+#import "FTOBBBPercentageStore.h"
+#import "SetData.h"
+#import "FTOBoringButBigAssistance.h"
 
 @implementation FTOBoringButBigViewController
 
-- (IBAction) percentageChanged: (id) sender {
+- (IBAction)percentageChanged:(id)sender {
     UITextField *percentageField = sender;
-
+    NSDecimalNumber *percentage = [NSDecimalNumber decimalNumberWithString:[percentageField text]];
+    [[[FTOBBBPercentageStore instance] first] setPercentage:percentage];
+    [[FTOBoringButBigAssistance new] setup];
 }
 
 @end
