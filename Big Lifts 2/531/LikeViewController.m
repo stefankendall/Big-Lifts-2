2e803c86b5f1478c0743077eda4806b98584aa0b
Stefan Kendall <stefankendall@gmail.com>
2013-09-18 13:43:24 -0400

    Removed sponsorship iap. Fixed simulator clean script

--- Big Lifts 2/531/LikeViewController.m
+++ Big Lifts 2/531/LikeViewController.m
@@ -1,25 +1,8 @@
 #import "LikeViewController.h"
 #import "FBAppCall.h"
 #import "FBDialogs.h"
-#import "Purchaser.h"
-#import "IAPAdapter.h"
-#import "PriceFormatter.h"
-#import "SKProductStore.h"
 
 @implementation LikeViewController
-
-- (void)viewDidLoad {
-    [self addPriceToCostLabel];
-}
-
-- (void)addPriceToCostLabel {
-    SKProduct *product = [[SKProductStore instance] productById:IAP_SPONSORSHIP];
-    NSString *priceText = [[PriceFormatter new] priceOf:product];
-    priceText = priceText == nil ? @"error" : priceText;
-    NSString *costText = [self.costParagraphLabel text];
-    [self.costParagraphLabel setText:
-            [costText stringByReplacingOccurrencesOfString:@"{cost}" withString:priceText]];
-}
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
