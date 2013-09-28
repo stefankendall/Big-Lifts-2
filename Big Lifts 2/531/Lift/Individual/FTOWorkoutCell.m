7eb646749692fb8e720635de8921dac9b298ae98
Stefan Kendall <stefankendall@gmail.com>
2013-09-28 13:34:14 -0400

    Removed cell modification in ftoworkoutcell. Moved setcell around to accomodate .5 roundings from 2.5 rounding

--- Big Lifts 2/531/Lift/Individual/FTOWorkoutCell.m
+++ Big Lifts 2/531/Lift/Individual/FTOWorkoutCell.m
@@ -20,23 +20,11 @@
     Class setClass = [[IAPAdapter instance] hasPurchased:IAP_BAR_LOADING] ? SetCellWithPlates.class : SetCell.class;
     self.setCell = [setClass create];
     UIView *contentView = self.setCell.contentView;
-    [self shiftContentViewLeftForDisclosure:contentView];
     [self addSubview:contentView];
-}
-
-- (void)shiftContentViewLeftForDisclosure:(UIView *)contentView {
-    int padding = 10;
-    contentView.bounds = CGRectInset(contentView.frame, padding, 0);
-    CGRect bounds = contentView.bounds;
-    [contentView setBounds:CGRectMake(bounds.origin.x - padding, bounds.origin.x, bounds.size.width, bounds.size.height)];
 }
 
 - (void)setSet:(Set *)set {
     [self.setCell setSet:set];
-
-    if ([set hasVariableReps]) {
-        [self setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
-    }
 }
 
 - (void)setSet:(Set *)set withEnteredReps:(NSNumber *)enteredReps {
