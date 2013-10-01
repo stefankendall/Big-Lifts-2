fa1f51c21404508cdf51793c2fc3b995af3c15ef
Stefan Kendall <stefankendall@gmail.com>
2013-09-30 21:01:45 -0400

    Moved Add button from bar button to bottom row.

--- Big Lifts 2/531/Edit/Change/FTOChangeLiftsViewController.m
+++ Big Lifts 2/531/Edit/Change/FTOChangeLiftsViewController.m
@@ -5,6 +5,7 @@
 #import "RowTextField.h"
 #import "UITableViewController+NoEmptyRows.h"
 #import "FTOWorkoutStore.h"
+#import "AddCell.h"
 
 @implementation FTOChangeLiftsViewController
 
@@ -12,21 +13,39 @@
     [self.tableView reloadData];
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [[FTOLiftStore instance] count];
+    if (section == 0) {
+        return [[FTOLiftStore instance] count];
+    }
+    else {
+        return 1;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    FTOChangeLiftCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(FTOChangeLiftCell.class)];
-    if (!cell) {
-        cell = [FTOChangeLiftCell create];
-    }
+    if ([indexPath section] == 0) {
+        FTOChangeLiftCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(FTOChangeLiftCell.class)];
+        if (!cell) {
+            cell = [FTOChangeLiftCell create];
+        }
 
-    Lift *lift = [self liftAtIndex:[indexPath row]];
-    [cell.textField setText:lift.name];
-    [cell.textField setDelegate:self];
-    [cell.textField setIndexPath:indexPath];
-    return cell;
+        Lift *lift = [self liftAtIndex:[indexPath row]];
+        [cell.textField setText:lift.name];
+        [cell.textField setDelegate:self];
+        [cell.textField setIndexPath:indexPath];
+        return cell;
+    }
+    else {
+        AddCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(AddCell.class)];
+        if (!cell) {
+            cell = [AddCell create];
+        }
+        return cell;
+    }
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -37,18 +56,39 @@
     }
 }
 
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([indexPath section] == 1) {
+        return UITableViewCellEditingStyleNone;
+    }
+    return UITableViewCellEditingStyleDelete;
+}
+
 - (void)textFieldDidEndEditing:(UITextField *)textField {
     RowTextField *rowTextField = (RowTextField *) textField;
     Lift *lift = [self liftAtIndex:[rowTextField.indexPath row]];
     [lift setName:[textField text]];
 }
 
-- (Lift *) liftAtIndex: (int) index {
+- (Lift *)liftAtIndex:(int)index {
     return [[FTOLiftStore instance] atIndex:index];
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
     return [self emptyView];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([indexPath section] == 1) {
+        [self performSegueWithIdentifier:@"ftoAddLiftSegue" sender:self];
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0;
 }
 
 @end
--- Big Lifts 2Tests/531/Edit/Change/FTOChangeLiftsViewControllerTests.m
+++ Big Lifts 2Tests/531/Edit/Change/FTOChangeLiftsViewControllerTests.m
@@ -7,17 +7,26 @@
 
 @implementation FTOChangeLiftsViewControllerTests
 
--(void) testHasRowsForAllLifts {
+- (void)testHasRowsForAllLifts {
     FTOChangeLiftsViewController *controller = [self getControllerByStoryboardIdentifier:@"ftoChangeLifts"];
     STAssertEquals([controller tableView:nil numberOfRowsInSection:0], 4, @"");
 }
 
--(void) testCanChangeLiftNames {
+- (void)testCanChangeLiftNames {
     FTOChangeLiftsViewController *controller = [self getControllerByStoryboardIdentifier:@"ftoChangeLifts"];
-    FTOChangeLiftCell * cell = (FTOChangeLiftCell *) [controller tableView:controller.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    FTOChangeLiftCell *cell = (FTOChangeLiftCell *) [controller tableView:controller.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
     [cell.textField setText:@"Power Clean"];
     [controller textFieldDidEndEditing:(id) cell.textField];
     STAssertEqualObjects([[[FTOLiftStore instance] first] name], @"Power Clean", @"");
+}
+
+- (void)testTappingAddRowCausesSegue {
+    FTOChangeLiftsViewController *controller = [self getControllerByStoryboardIdentifier:@"ftoChangeLifts"];
+    UINavigationController *nav = [UINavigationController new];
+    [nav pushViewController:controller animated:NO];
+    int viewCount = [controller.navigationController.viewControllers count];
+    [controller tableView:controller.tableView didSelectRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:1]];
+    STAssertEquals([controller.navigationController.viewControllers count], (NSUInteger) viewCount + 1, @"");
 }
 
 @end
