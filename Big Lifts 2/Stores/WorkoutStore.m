4a4add877cccd7755cf963a6aa33cd0e57476798
Stefan Kendall <stefankendall@gmail.com>
2013-10-22 20:58:04 -0400

    Order is now added to sets on startup.

--- Big Lifts 2/Stores/WorkoutStore.m
+++ Big Lifts 2/Stores/WorkoutStore.m
@@ -1,4 +1,24 @@
 #import "WorkoutStore.h"
+#import "Workout.h"
+#import "Lift.h"
+#import "Set.h"
 
 @implementation WorkoutStore
+
+- (void)onLoad {
+    [self correctEmptyOrderOnSets];
+}
+
+- (void)correctEmptyOrderOnSets {
+    NSArray *allWorkouts = [self findAll];
+    for (Workout *workout in allWorkouts) {
+        if ([[workout.sets firstObject] order] == nil ) {
+            int count = 0;
+            for (Set *set in workout.sets) {
+                set.order = [NSNumber numberWithInt:count++];
+            }
+        }
+    }
+}
+
 @end
--- Big Lifts 2Tests/Stores/WorkoutStoreTests.m
+++ Big Lifts 2Tests/Stores/WorkoutStoreTests.m
@@ -6,13 +6,29 @@
 
 @implementation WorkoutStoreTests
 
--(void) testSetsOrderOnSetsWhenAdding {
+- (void)testSetsOrderOnSetsWhenAdding {
     Workout *workout = [[WorkoutStore instance] create];
     Set *set1 = [[SetStore instance] create];
     Set *set2 = [[SetStore instance] create];
     Set *set3 = [[SetStore instance] create];
 
     [workout addSets:@[set1, set2, set3]];
+    STAssertEquals([set1.order intValue], 0, @"" );
+    STAssertEquals([set2.order intValue], 1, @"" );
+    STAssertEquals([set3.order intValue], 2, @"" );
+}
+
+- (void)testFixesSetsWithBrokenOrderingOnLoad {
+    Workout *workout = [[WorkoutStore instance] create];
+    Set *set1 = [[SetStore instance] create];
+    Set *set2 = [[SetStore instance] create];
+    Set *set3 = [[SetStore instance] create];
+    [workout addSets:@[set1, set2, set3]];
+    set1.order = nil;
+    set2.order = nil;
+    set3.order = nil;
+
+    [[WorkoutStore instance] onLoad];
     STAssertEquals([set1.order intValue], 0, @"" );
     STAssertEquals([set2.order intValue], 1, @"" );
     STAssertEquals([set3.order intValue], 2, @"" );
