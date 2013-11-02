9da908238d00427f5e606db6d0c67de05de8cf28
Stefan Kendall <stefankendall@gmail.com>
2013-11-02 12:43:02 -0400

    Lightened toolbar background.

--- Big Lifts 2/Components/TextViewInputAccessoryBuilder.m
+++ Big Lifts 2/Components/TextViewInputAccessoryBuilder.m
@@ -5,7 +5,7 @@
 - (UIView *)doneButtonAccessory:(UITextField *)textField {
     UIBarButtonItem *barButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:textField action:@selector(resignFirstResponder)];
     UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
-    [toolbar setBarTintColor:[UIColor lightGrayColor]];
+    [toolbar setBarTintColor:[UIColor colorWithRed:214/255.0f green:214/255.0f blue:214/255.0f alpha:1.0f]];
     toolbar.items = @[barButton];
 
     textField.inputAccessoryView = toolbar;
