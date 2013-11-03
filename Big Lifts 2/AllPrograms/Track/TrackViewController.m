db1fe1f314e8f3c3096aa9bacdda5f35b7b2bbe2
Stefan Kendall <stefankendall@gmail.com>
2013-11-03 08:37:02 -0500

    Fixed incorrect cell re-use.

--- Big Lifts 2/AllPrograms/Track/TrackViewController.m
+++ Big Lifts 2/AllPrograms/Track/TrackViewController.m
@@ -57,8 +57,8 @@
         WorkoutLogCell *cell = (WorkoutLogCell *) [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(WorkoutLogCell.class)];
         if (!cell) {
             cell = [WorkoutLogCell create];
-            [cell setWorkoutLog:[self getLog][(NSUInteger) [indexPath row]]];
         }
+        [cell setWorkoutLog:[self getLog][(NSUInteger) [indexPath row]]];
         return cell;
     }
 }
