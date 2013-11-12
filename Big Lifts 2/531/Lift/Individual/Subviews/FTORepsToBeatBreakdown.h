0936298bba4808310419d981415cd83a66ab5f90
Stefan Kendall <stefankendall@gmail.com>
2013-11-11 21:07:11 -0500

    Added config selector for reps to beat.

--- Big Lifts 2/531/Lift/Individual/Subviews/FTORepsToBeatBreakdown.h
+++ Big Lifts 2/531/Lift/Individual/Subviews/FTORepsToBeatBreakdown.h
@@ -1,12 +1,16 @@
 @class Set;
+@class PaddingTextField;
 
-@interface FTORepsToBeatBreakdown : UITableViewController {
+@interface FTORepsToBeatBreakdown : UITableViewController <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate> {
 }
 @property(weak, nonatomic) IBOutlet UILabel *enteredOneRepMax;
 @property(weak, nonatomic) IBOutlet UILabel *maxFromLog;
 @property(weak, nonatomic) IBOutlet UILabel *reps;
 @property(weak, nonatomic) IBOutlet UILabel *weight;
 @property(weak, nonatomic) IBOutlet UILabel *estimatedMax;
+@property (weak, nonatomic) IBOutlet PaddingTextField *configTextField;
 
 @property(nonatomic, strong) Set *lastSet;
+@property(nonatomic, strong) UIPickerView *configPicker;
+@property(nonatomic, strong) NSArray *configOptions;
 @end
--- Big Lifts 2/531/Lift/Individual/Subviews/FTORepsToBeatBreakdown.m
+++ Big Lifts 2/531/Lift/Individual/Subviews/FTORepsToBeatBreakdown.m
@@ -3,18 +3,31 @@
 #import "Set.h"
 #import "OneRepEstimator.h"
 #import "Lift.h"
+#import "PaddingTextField.h"
+#import "TextViewInputAccessoryBuilder.h"
 
 @implementation FTORepsToBeatBreakdown
+
+- (void)viewDidLoad {
+    self.configOptions = @[@"Use Everything", @"Log Only"];
+
+    [[TextViewInputAccessoryBuilder new] doneButtonAccessory:self.configTextField];
+    [self.configTextField setDelegate:self];
+    self.configPicker = [UIPickerView new];
+    [self.configPicker setDataSource:self];
+    [self.configPicker setDelegate:self];
+    self.configTextField.inputView = self.configPicker;
+}
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 
     [self.enteredOneRepMax setText:[self.lastSet.lift.weight stringValue]];
-    NSDecimalNumber *logMax = [[FTORepsToBeatCalculator new] findLogMax:self.lastSet.lift];
+    NSDecimalNumber *logMax = [[FTORepsToBeatCalculator new] findLogMax:(id) self.lastSet.lift];
     [self.maxFromLog setText:[logMax stringValue]];
 
     NSDecimalNumber *lastSetWeight = [self.lastSet roundedEffectiveWeight];
-    int minimumReps = [[FTORepsToBeatCalculator new] repsToBeat:self.lastSet.lift atWeight:lastSetWeight];
+    int minimumReps = [[FTORepsToBeatCalculator new] repsToBeat:(id) self.lastSet.lift atWeight:lastSetWeight];
     [self.reps setText:[NSString stringWithFormat:@"%dx", minimumReps]];
     [self.weight setText:[lastSetWeight stringValue]];
 
@@ -22,5 +35,20 @@
     [self.estimatedMax setText:[estimatedMax stringValue]];
 }
 
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [self.configOptions count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.configOptions[(NSUInteger) row];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    [self.configTextField setText:self.configOptions[(NSUInteger) [self.configPicker selectedRowInComponent:0]]];
+}
 
 @end
