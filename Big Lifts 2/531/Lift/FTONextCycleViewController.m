c02ebc6fe004a1e0ea09883922fceb79295cb303
Stefan Kendall <stefankendall@gmail.com>
2013-12-10 16:44:11 -0500

    Manually ending a cycle now increments lifts.

--- Big Lifts 2/531/Lift/FTONextCycleViewController.m
+++ Big Lifts 2/531/Lift/FTONextCycleViewController.m
@@ -1,10 +1,12 @@
 #import "FTONextCycleViewController.h"
 #import "FTOCycleAdjustor.h"
+#import "JFTOLiftStore.h"
 
 @implementation FTONextCycleViewController
 - (IBAction)doneButtonTapped:(id)selector {
     FTOCycleAdjustor *cycleAdjustor = [FTOCycleAdjustor new];
     [cycleAdjustor nextCycle];
+    [[JFTOLiftStore instance] incrementLifts];
     [self.navigationController popViewControllerAnimated:YES];
 }
 @end
