b68e89703d275b7994d3ad223d96ebc163c5dacf
Stefan Kendall <stefankendall@gmail.com>
2013-12-13 20:00:21 -0500

    Removed no longer needed MigrateDataViewController.

--- Big Lifts 2/MigrateDataViewController.h
+++
@@ -1,8 +0,0 @@
-@interface MigrateDataViewController : UIViewController {}
-- (void)markHasSeenMigrationPrompt:(BOOL)hasSeen;
-
-- (BOOL)hasSeenMigrationPrompt;
-@property (weak, nonatomic) IBOutlet UIButton *migrateDataButton;
-@property (weak, nonatomic) IBOutlet UIButton *startFreshButton;
-
-@end
