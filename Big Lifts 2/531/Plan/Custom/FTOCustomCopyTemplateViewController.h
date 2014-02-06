05453572ef99c160e17ef3d20c987db2baf58152
Stefan Kendall <stefankendall@gmail.com>
2014-02-05 21:38:22 -0500

    Fixed bad tableview cell outlets.

--- Big Lifts 2/531/Plan/Custom/FTOCustomCopyTemplateViewController.h
+++ Big Lifts 2/531/Plan/Custom/FTOCustomCopyTemplateViewController.h
@@ -1,9 +1,5 @@
-@interface FTOCustomCopyTemplateViewController : UITableViewController {}
-@property (weak, nonatomic) IBOutlet UITableViewCell *standard;
-@property (weak, nonatomic) IBOutlet UITableViewCell *pyramid;
-@property (weak, nonatomic) IBOutlet UITableViewCell *joker;
-@property (weak, nonatomic) IBOutlet UITableViewCell *advanced;
-
+@interface FTOCustomCopyTemplateViewController : UITableViewController {
+}
 @property(nonatomic, strong) NSDictionary *textToVariant;
 @property(nonatomic, strong) NSDictionary *iapVariants;
 @property(nonatomic, strong) NSArray *orderedVariants;
