9fae40033a28c86b5c2b822aca1d69f690fb7f79
Stefan Kendall <stefankendall@gmail.com>
2014-02-15 09:55:53 -0500

    Completed wilks calculator. Need to wire.

--- Big Lifts 2/AllPrograms/Formulas/WilksCoefficientCalculator.m
+++ Big Lifts 2/AllPrograms/Formulas/WilksCoefficientCalculator.m
@@ -3,28 +3,46 @@
 
 @implementation WilksCoefficientCalculator
 
-+ (NSDecimalNumber *)calculate:(NSDecimalNumber *)weight withBodyweight:(NSDecimalNumber *)bodyweight isMale:(BOOL)isMale withUnits: (NSString *) units {
++ (NSDecimalNumber *)calculate:(NSDecimalNumber *)weight withBodyweight:(NSDecimalNumber *)bodyweight isMale:(BOOL)isMale withUnits:(NSString *)units {
     if (weight == nil || bodyweight == nil ) {
         return N(0);
     }
 
-    NSDecimalNumber *liftedWeight = [units isEqualToString:@"kg"] ? weight : [WeightUnits lbsToKg:weight];
-    NSDictionary *coefficients = isMale ? @{
-            @"a": N(-216.0475144),
-            @"b": N(16.2606339),
-            @"c": N(-0.002388645),
-            @"d": N(-0.00113732),
-            @"e": N(0.00000701863),
-            @"f": N(-0.00000001291)
+    NSDictionary *k = isMale ? @{
+            @"a" : N(-216.0475144),
+            @"b" : N(16.2606339),
+            @"c" : N(-0.002388645),
+            @"d" : N(-0.00113732),
+            @"e" : N(0.00000701863),
+            @"f" : N(-0.00000001291)
     } : @{
-            @"a": N(594.31747775582),
-            @"b": N(-27.23842536447),
-            @"c": N(0.82112226871),
-            @"d": N(-0.00930733913),
-            @"e": N(0.00004731582),
-            @"f": N(-0.00000009054)
+            @"a" : N(594.31747775582),
+            @"b" : N(-27.23842536447),
+            @"c" : N(0.82112226871),
+            @"d" : N(-0.00930733913),
+            @"e" : N(0.00004731582),
+            @"f" : N(-0.00000009054)
     };
-    return liftedWeight;
+    NSDecimalNumber *convertedBodyweight = [units isEqualToString:@"kg"] ? bodyweight : [WeightUnits lbsToKg:bodyweight];
+    NSArray *terms = @[
+            k[@"a"],
+            [k[@"b"] decimalNumberByMultiplyingBy:convertedBodyweight],
+            [k[@"c"] decimalNumberByMultiplyingBy:[convertedBodyweight decimalNumberByRaisingToPower:2]],
+            [k[@"d"] decimalNumberByMultiplyingBy:[convertedBodyweight decimalNumberByRaisingToPower:3]],
+            [k[@"e"] decimalNumberByMultiplyingBy:[convertedBodyweight decimalNumberByRaisingToPower:4]],
+            [k[@"f"] decimalNumberByMultiplyingBy:[convertedBodyweight decimalNumberByRaisingToPower:5]]
+    ];
+
+    NSDecimalNumber *denominator = N(0);
+    for (NSDecimalNumber *term in terms) {
+        denominator = [denominator decimalNumberByAdding:term];
+    }
+
+    NSDecimalNumber *coefficient = [N(500) decimalNumberByDividingBy:denominator];
+    NSDecimalNumber *convertedLiftedWeight = [units isEqualToString:@"kg"] ? weight : [WeightUnits lbsToKg:weight];
+    return [[coefficient decimalNumberByMultiplyingBy:convertedLiftedWeight] decimalNumberByRoundingAccordingToBehavior:
+            [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundPlain scale:3 raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:NO]
+    ];
 }
 
 @end
--- Big Lifts 2Tests/AllPrograms/Formulas/WilksCoefficientCalculatorTests.m
+++ Big Lifts 2Tests/AllPrograms/Formulas/WilksCoefficientCalculatorTests.m
@@ -4,21 +4,28 @@
 @implementation WilksCoefficientCalculatorTests
 
 - (void)testCalculatesWilks {
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(170) isMale:YES withUnits:@"kg"], N(162.93), @"");
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(150) isMale:YES withUnits:@"kg"], N(165.99), @"");
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(200) withBodyweight:N(150) isMale:YES withUnits:@"kg"], N(110.66), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(170) isMale:YES withUnits:@"kg"], N(162.943), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(150) isMale:YES withUnits:@"kg"], N(165.989), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(200) withBodyweight:N(150) isMale:YES withUnits:@"kg"], N(110.659), @"");
 }
 
 - (void)testCalculatesWilksLbs {
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(170) isMale:YES withUnits:@"lbs"], N(95.146), @"");
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(150) isMale:YES withUnits:@"lbs"], N(104.249), @"");
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(200) withBodyweight:N(150) isMale:YES withUnits:@"lbs"], N(69.499), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(170) isMale:YES withUnits:@"lbs"], N(95.145), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(150) isMale:YES withUnits:@"lbs"], N(104.256), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(200) withBodyweight:N(150) isMale:YES withUnits:@"lbs"], N(69.504), @"");
 }
 
 - (void)testCalculatesForFemale {
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(170) isMale:NO withUnits:@"kg"], N(237.9), @"");
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(150) isMale:NO withUnits:@"kg"], N(230.85), @"");
-    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(200) withBodyweight:N(150) isMale:NO withUnits:@"kg"], N(153.9), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(170) isMale:NO withUnits:@"kg"], N(237.898), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(300) withBodyweight:N(150) isMale:NO withUnits:@"kg"], N(230.845), @"");
+    STAssertEqualObjects([WilksCoefficientCalculator calculate:N(200) withBodyweight:N(150) isMale:NO withUnits:@"kg"], N(153.896), @"");
+}
+
+- (void)testDoesNotCrashBoundaryValues {
+    [WilksCoefficientCalculator calculate:nil withBodyweight:nil isMale:NO withUnits:@"kg"];
+    [WilksCoefficientCalculator calculate:nil withBodyweight:nil isMale:NO withUnits:@"lbs"];
+    [WilksCoefficientCalculator calculate:nil withBodyweight:nil isMale:YES withUnits:@"lbs"];
+    [WilksCoefficientCalculator calculate:N(0) withBodyweight:N(0) isMale:YES withUnits:@"lbs"];
 }
 
 @end
