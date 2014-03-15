1825aeb6c3dcc6aaf9faecb1dedbd41b4c8b22b1
Stefan Kendall <stefankendall@gmail.com>
2014-03-15 18:53:47 -0400

    Added tests for data.

---
+++ Big Lifts 2Tests/JStores/FTO/JFTOFullCustomWeekStoreTests.m
@@ -0,0 +1,26 @@
+#import "JFTOFullCustomWeekStoreTests.h"
+#import "JFTOFullCustomWeekStore.h"
+#import "JFTOFullCustomWeek.h"
+
+@implementation JFTOFullCustomWeekStoreTests
+
+- (void)testHasWeekNames {
+    JFTOFullCustomWeek *customWeek = [[JFTOFullCustomWeekStore instance] find:@"week" value:@1];
+    STAssertEqualObjects(customWeek.name, @"5/5/5", @"");
+}
+
+- (void)testSetsWeekIncrement {
+    STAssertFalse([[[JFTOFullCustomWeekStore instance] find:@"week" value:@1] incrementAfterWeek], @"");
+    STAssertFalse([[[JFTOFullCustomWeekStore instance] find:@"week" value:@2] incrementAfterWeek], @"");
+    STAssertFalse([[[JFTOFullCustomWeekStore instance] find:@"week" value:@3] incrementAfterWeek], @"");
+    STAssertTrue([[[JFTOFullCustomWeekStore instance] find:@"week" value:@4] incrementAfterWeek], @"");
+}
+
+- (void)testHasCustomWorkouts {
+    JFTOFullCustomWeek *customWeek1 = [[JFTOFullCustomWeekStore instance] find:@"week" value:@1];
+    JFTOFullCustomWeek *customWeek2 = [[JFTOFullCustomWeekStore instance] find:@"week" value:@2];
+    STAssertEquals((int) [customWeek1.workouts count], 4, @"");
+    STAssertEquals((int) [customWeek2.workouts count], 4, @"");
+}
+
+@end
