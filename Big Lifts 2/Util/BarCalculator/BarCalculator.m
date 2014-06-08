f563d6343dcf33152ea1d8bd949491736c72064a
Stefan Kendall <stefankendall@gmail.com>
2014-06-08 16:29:07 -0400

    Fixed bar calculation when smaller plates summed could make a closer weight than a sum of larger waeights.

--- Big Lifts 2/Util/BarCalculator/BarCalculator.m
+++ Big Lifts 2/Util/BarCalculator/BarCalculator.m
@@ -24,6 +24,7 @@
     NSArray *remainingPlates = [self copyPlates:self.plates];
     NSMutableArray *plateWeights = [@[] mutableCopy];
 
+    PlateRemaining *lastPlate = nil;
     while ([targetWeight compare:N(0)] == NSOrderedDescending) {
         remainingPlates = [remainingPlates select:^(PlateRemaining *p) {
             BOOL countRemaining = p.count > 0;
@@ -31,7 +32,17 @@
         }];
 
         PlateRemaining *nextPlate = [self findPlateClosestToWeight:targetWeight fromPlates:remainingPlates];
-        if (nextPlate == nil ) {
+        if (nextPlate == nil) {
+            if (![targetWeight isEqual:N(0)]) {
+                NSArray *platesWithoutLastPlate = [self.plates select:^BOOL(PlateRemaining *plate) {
+                    return ![plate.weight isEqual:lastPlate.weight];
+                }];
+                if ([platesWithoutLastPlate count] > 0) {
+                    BarCalculator *calculatorSkippingThisWeight = [[BarCalculator new] initWithPlates:platesWithoutLastPlate barWeight:self.barWeight];
+                    NSArray *possibleOtherPlates = [calculatorSkippingThisWeight platesToMakeWeight:weight];
+                    plateWeights = [[self platesCloserTo:targetWeight plates1:plateWeights plates2:possibleOtherPlates] mutableCopy];
+                }
+            }
             break;
         }
         else {
@@ -40,9 +51,39 @@
             targetWeight = [targetWeight decimalNumberBySubtracting:
                     [nextPlate.weight decimalNumberByMultiplyingBy:N(2)]];
         }
+        lastPlate = nextPlate;
     }
 
     return plateWeights;
+}
+
+- (NSArray *)platesCloserTo:(NSDecimalNumber *)target plates1:(NSMutableArray *)plates1 plates2:(NSArray *)plates2 {
+    __block NSDecimalNumber *sum1 = N(0);
+    [plates1 each:^(NSDecimalNumber *weight) {
+        sum1 = [sum1 decimalNumberByAdding:[weight decimalNumberByMultiplyingBy:N(2)]];
+    }];
+
+    __block NSDecimalNumber *sum2 = N(0);
+    [plates2 each:^(NSDecimalNumber *weight) {
+        sum2 = [sum1 decimalNumberByAdding:[weight decimalNumberByMultiplyingBy:N(2)]];
+    }];
+
+    NSDecimalNumber *diff1 = [target decimalNumberBySubtracting:sum1];
+    NSDecimalNumber *diff2 = [target decimalNumberBySubtracting:sum2];
+
+    if ([diff1 compare:N(0)] == NSOrderedAscending) {
+        return plates2;
+    }
+    if ([diff2 compare:N(0)] == NSOrderedAscending) {
+        return plates1;
+    }
+
+    if ([diff1 compare:diff2] == NSOrderedAscending) {
+        return plates1;
+    }
+    else {
+        return plates2;
+    }
 }
 
 - (PlateRemaining *)findPlateClosestToWeight:(NSDecimalNumber *)weight fromPlates:(NSArray *)plates {
@@ -52,10 +93,10 @@
     }];
 }
 
-- (NSArray *)copyPlates:(NSArray *)plates {
-    return [plates collect:(^(JPlate *p) {
+- (NSMutableArray *)copyPlates:(NSArray *)plates {
+    return [[plates collect:(^(JPlate *p) {
         return [PlateRemaining fromPlate:p];
-    })];
+    })] mutableCopy];
 }
 
 @end
