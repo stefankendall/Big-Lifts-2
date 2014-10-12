aaeff1d963dc13a4f1f7968f50b6aa49b26bb530
Stefan Kendall <stefankendall@gmail.com>
2014-10-12 16:40:05 -0400

    Put saving in a long-running task.

--- Big Lifts 2/BLAppDelegate.m
+++ Big Lifts 2/BLAppDelegate.m
@@ -50,21 +50,25 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     [[BLTimer instance] suspend];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    [[BLJStoreManager instance] syncStores];
+    UIBackgroundTaskIdentifier __block bgTask = nil;
+    UIApplication *app = [UIApplication sharedApplication];
+    bgTask = [app beginBackgroundTaskWithExpirationHandler:^{
+        [app endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
+    }];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [[BLJStoreManager instance] syncStores];
+    });
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     [[BLTimer instance] resume];
 }
 
